17673c1e06cd3fcd439ec0b494aa55861a95a5c1
John Angelo Midel <j.midel.488910@umindanao.edu.ph>
2023-04-25 20:53:18 +0800

    1st Testing

---
+++ js/script2.js
@@ -0,0 +1,49 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAs_aA90RCE_mGD3KlBDs4G0MkSorGS2FA",
+	authDomain: "micro-realtimedatabase.firebaseapp.com",
+	databaseURL: "https://micro-realtimedatabase-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "micro-realtimedatabase",
+	storageBucket: "micro-realtimedatabase.appspot.com",
+	messagingSenderId: "711979305578",
+	appId: "1:711979305578:web:0b7f4c01fe2091f6e99813",
+	measurementId: "G-P9NDH3FFXV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+$(document).ready(function(){
+	
+    const database = firebase.database();
+	var firebaseRef = firebase.database().ref().child("LDRValue");
+	document.getElementById("water-level").innerHTML = LDRValue;
+	//var Led1Status;
+	const ldrData;
+
+	database.ref().on("value", function(snap){
+		// Led1Status = snap.val().Led1Status;
+
+		// if(Led1Status == "1"){    // check from the firebase
+		// 	//$(".Light1Status").text("The light is off");
+		// 	document.getElementById("unact").style.display = "none";
+		// 	document.getElementById("act").style.display = "block";
+		// } else {
+		// 	//$(".Light1Status").text("The light is on");
+		// 	document.getElementById("unact").style.display = "block";
+		// 	document.getElementById("act").style.display = "none";
+		// }
+	});
+
+	
+
+    // $(".toggle-btn").click(function(){
+	// 	var firebaseRef = firebase.database().ref().child("LDRValue");
+
+	// 	if(Led1Status == "1"){    // post to firebase
+	// 		firebaseRef.set("0");
+	// 		Led1Status = "0";
+	// 	} else {
+	// 		firebaseRef.set("1");
+	// 		Led1Status = "1";
+	// 	}
+	// })
+});
