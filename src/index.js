6309cb5ae8278321aba38ad2534a38b180eb361a
John Angelo Midel <j.midel.488910@umindanao.edu.ph>
2023-04-29 23:44:02 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -16,56 +16,6 @@ function closeSidebar() {
         sidebar.classList.remove("sidebar-responsive");
         sidebarOpen = false;
     }
-    
 }
 
-// ----------CHARTS-------------
-
-// BAR Graph
-
-var barChartOptions = {
-    series: [{
-    data: [10, 8, 6, 4, 2],
-    name: "Products"
-  }],
-    chart: {
-    type: 'bar',
-    height: 350,
-    toolbar:{
-        show: false,
-    },
-  },
-  colors:[
-    "#2962ff",
-    "#d50000",
-    "#2e7d32",
-    "#ff6d00",
-    "#583cb3",
-  ],
-  plotOptions: {
-    bar: {
-      borderRadius: 4,
-      horizontal: true,
-      horizontal: false,
-      columnWidth: "40%",
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  xaxis: {
-    categories: ['Laptop', 'Phone', 'Monitor', 'Headphones', 'Camera', 
-    ],
-  },
-  yaxis:{
-    title:{
-        text: "Count",
-        style:{
-            color: "#f5f7ff",
-        },
-    },
-  }
-  };
-
-  var chart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
-  chart.render();
+// ----------FIREBASE-------------
